476660f63a4ff1c8a5e971b7347d124c76fb0729
dekadentno <matej.lazarevich@gmail.com>
2020-03-25 15:55:17 +0100

    Join topic page style

--- pages/join.vue
+++ pages/join.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
-    join a topic
-    <input v-model="topicId" type="text">
+  <div class="join-topic">
+    <input
+      ref="topic"
+      v-model="topicId"
+      placeholder="Enter your topic ID"
+      type="text"
+      class="text-input"
+    >
+    <div class="actions">
+      <button class="btn btn--primary" @click="joinTopic">
+        join topic
+      </button>
+      <button class="btn btn--secondary" @click="goHome">
+        back
+      </button>
+    </div>
   </div>
 </template>
 
@@ -16,10 +29,32 @@ export default {
     return {
       topicId: ''
     }
+  },
+  methods: {
+    joinTopic () {
+      this.$router.push('/topic/' + this.topicId)
+    },
+    goHome () {
+      this.$router.push('/')
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.join-topic {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
 
+  .actions {
+    margin-top: 20px;
+
+    .btn {
+      margin: 0 10px;
+    }
+  }
+}
 </style>
